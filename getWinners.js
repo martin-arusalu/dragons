740639634404d33fcbf24ca7dc9cddcc9c5bc5a9
Martin Arusalu <martin.arusalu@gmail.com>
2018-04-23 12:19:19 +0300

    Add possibility to copy

--- getWinners.js
+++ getWinners.js
@@ -1,6 +1,28 @@
 
 let winners = [];
 let dragons = [];
+const jsonElem = document.getElementById('json');
+
+// REFERENCE: https://hackernoon.com/copying-text-to-clipboard-with-javascript-df4d4988697f
+const copyToClipboard = () => {
+  const el = document.createElement('textarea');  // Create a <textarea> element
+  el.value = JSON.stringify(winners);                                 // Set its value to the string that you want copied
+  el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+  el.style.position = 'absolute';                 
+  el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+  document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+  const selected =            
+    document.getSelection().rangeCount > 0        // Check if there is any content selected previously
+      ? document.getSelection().getRangeAt(0)     // Store selection if found
+      : false;                                    // Mark as false to know no selection existed before
+  el.select();                                    // Select the <textarea> content
+  document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+  document.body.removeChild(el);                  // Remove the <textarea> element
+  if (selected) {                                 // If a selection existed before copying
+    document.getSelection().removeAllRanges();    // Unselect everything on the HTML document
+    document.getSelection().addRange(selected);   // Restore the original selection
+  }
+};
 
 const fetchOldWinners = async () => {
   await fetch('https://raw.githubusercontent.com/martin-arusalu/dragons/master/winners.json')
@@ -56,6 +78,7 @@ const generateWinners = async num => {
             if (result) {
               console.log('found one');
               winners.push({ knight, dragon });
+              jsonElem.innerHTML = winners.length;
               console.log(winners);
               break;
             }
